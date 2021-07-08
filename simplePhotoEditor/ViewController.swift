9a4671288067c63f6023e74238e3f12ed23d5192
Andrey Kim <akart.dev@gmail.com>
2021-07-08 15:22:22 +0600

    initial commit

---
+++ simplePhotoEditor/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  simplePhotoEditor
+//
+//  Created by Andrey Kim on 26.06.2021.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate {
+
+    @IBOutlet weak var addButton: UIButton!
+    
+    @IBOutlet weak var picture: UIImageView!
+    
+    private var imagePicker: ImagePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.imagePicker = ImagePicker(presentationController: self, delegate: self)
+    }
+
+    @IBAction func editAction(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Editor", bundle: nil)
+        let vc = storyboard.instantiateViewController(identifier: "Editor") as! EditorViewController
+        vc.picture = self.picture.image
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @IBAction func addAction(_ sender: Any) {
+    
+    self.imagePicker.present(from: sender as! UIView)
+    }
+    
+}
+
+extension ViewController: ImagePickerDelegate {
+    func didSelect(image: UIImage?) {
+        self.picture.image = image
+    }
+}
